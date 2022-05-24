6a5f254abb5b9c99a2b489d3f5dd1ec352113edd
Danylo <antoni@everywhere.pl>
2022-05-24 16:57:30 +0200

    site-header

--- assets/js/sub/scrollEvents.ts
+++ assets/js/sub/scrollEvents.ts
@@ -1,5 +1,11 @@
 export default function scrollEvents(){
     document.addEventListener('scroll', function(e) {
+        // Site Header
+        let siteHeader = document.querySelector('[data-site_header]');
+        let scrollPosition = window.scrollY;
+        if (scrollPosition >= 30) siteHeader.classList.add('scrolled')
+        else siteHeader.classList.remove('scrolled');
+        // Banner Arrow
         let banerArrow = document.querySelector('[data-baner_arrow]');
         let banerArrowHidden = false;
         if (banerArrow && !banerArrowHidden){
