37e777a0cde1c9a251941d01c9474aeed26d9787
Danylo <antoni@everywhere.pl>
2022-05-27 11:03:46 +0200

    Stopka
    Also mainpage is fully done

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,12 +6,8 @@ $functions/php-add-scripts-styles.php,0\1\0197555521b15da56a7bb1d853cdea27f2438
 :
 
 header.php,6\3\6314feee14ea09173c34632864f7659d9d5a5a84
-K
-functions/php-allow-svg.php,b\2\b2bf48ac1d74a0ab60c67a5f75073f1acc3b10ab
 O
 functions/php-remove-editor.php,2\c\2cd9e2181897130ea0d82a4ca3cfab6cd4d89fc2
-O
-components/component-footer.php,5\f\5f783efc14b5c8dcf87b4004ddf8a3639bbb3d45
 P
  assets/images/svg/arrow-down.svg,9\e\9ed5eb83260403bae3faced1d5811a5e41bc714f
 O
--- components/component-footer.php
+++ components/component-footer.php
@@ -1,4 +1,69 @@
 <footer>
     <?php get_template_part('/components/component-intro'); ?>
+    <section class="site-footer">
+        <?php
+        $banner = get_field('footer_banner', 'options');
+        $content = get_field('tresci_stopki', 'options');
+        $col_1 = $content['col_1'];
+        $col_2 = $content['col_2'];
+        $col_3 = $content['col_3'];
+        $sm_links = get_field('sm_links', 'options');
+        $pp_link = get_field('pp_link', 'options')
+        ?>
+        <div class="site-footer__banner">
+            <img src="<?php echo $banner ?>" alt="Słoneczna Rezydencja">
+        </div>
+        <div class="row mx-0 px-0 pt-5 pb-4">
+            <div class="col-12 col-md-10 offset-md-1">
+                <div class="site-footer__wrapper text-center">
+                    <img src="<?php echo THEME_URI . '/assets/images/footer-logo.jpg' ?>" alt="Rezydencja Słoneczna">
+                    <div class="row mt-5">
+                        <div class="col-12 col-md-4">
+                            <h4 class="content__title text-center">
+                                <?php echo $col_1['naglowek']; ?>
+                            </h4>
+                            <div class="content__text text-center">
+                                <?php echo $col_1['tresc']; ?>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <h4 class="content__title text-center">
+                                <?php echo $col_2['naglowek']; ?>
+                            </h4>
+                            <div class="content__text text-center">
+                                <?php echo $col_2['tresc']; ?>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <h4 class="content__title text-center">
+                                <?php echo $col_3['naglowek']; ?>
+                            </h4>
+                            <div class="content__text text-center">
+                                <?php echo $col_3['tresc']; ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="site-footer__sm mt-3">
+                        <?php foreach ($sm_links as $link) : ?>
+                        <a href="<?php echo $link['link'] ?>" target="_blank" class="site-footer__sm--item">
+                            <img src="<?php echo $link['ikonka'] ?>" alt="Social Media">
+                        </a>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="site-footer__copyright mt-4 pt-4">
+                        <a href="<?php echo $pp_link ?>" class="site-footer__copyright--pp">
+                            Polityka Prywatności
+                        </a>
+                        <div class="site-footer__copyright--everywhere">
+                            <p class="site-footer__copyright--text">© 2022 Słoneczna Rezydencja Wszelkie prawa zastrzeżone.</p>
+                            <a class="site-footer__copyright--link" href="https://everywhere.pl/">
+                                <img src="<?php echo THEME_URI.'/assets/images/everywhere.jpg' ?>" class="site-footer__copyright--img" alt="Created By: Everywhere Polska">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <?php wp_footer(); ?>
 </footer>
